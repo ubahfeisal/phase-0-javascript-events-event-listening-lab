06373ae6598a3ed1073bf8a351d2f8020ebc8c78
ubah feisal <feisalubah1@gmail.com>
2023-06-24 10:05:45 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    input.addEventListener('click', clickAlert);
 }
+// const input = document.getElementById('input');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
